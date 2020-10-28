a3d68a206c7a409baa0df6d21ac1ee646dea2406
eli <zhangxuan1107@163.com>
2020-10-28 10:28:17 +0800

    添加项目文件。

---
+++ UcLog.Designer.cs
@@ -0,0 +1,182 @@
+﻿using System.Windows.Forms;
+
+namespace FTUc
+{
+    partial class UcLog
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupControl1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiltter2 = new System.Windows.Forms.TextBox();
+            this.txtFiltter = new System.Windows.Forms.TextBox();
+            this.comboBoxEdit1 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.labelControl1 = new System.Windows.Forms.Label();
+            this.checkEdit1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(584, 344);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.txtFiltter2);
+            this.groupControl1.Controls.Add(this.txtFiltter);
+            this.groupControl1.Controls.Add(this.comboBoxEdit1);
+            this.groupControl1.Controls.Add(this.linkLabel1);
+            this.groupControl1.Controls.Add(this.labelControl1);
+            this.groupControl1.Controls.Add(this.richTextBox1);
+            this.groupControl1.Controls.Add(this.checkEdit1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(593, 386);
+            this.groupControl1.TabIndex = 4;
+            this.groupControl1.TabStop = false;
+            this.groupControl1.Text = "日志信息";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "并且";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "过滤:";
+            // 
+            // txtFiltter2
+            // 
+            this.txtFiltter2.Location = new System.Drawing.Point(487, 13);
+            this.txtFiltter2.Name = "txtFiltter2";
+            this.txtFiltter2.Size = new System.Drawing.Size(100, 21);
+            this.txtFiltter2.TabIndex = 10;
+            // 
+            // txtFiltter
+            // 
+            this.txtFiltter.Location = new System.Drawing.Point(353, 13);
+            this.txtFiltter.Name = "txtFiltter";
+            this.txtFiltter.Size = new System.Drawing.Size(100, 21);
+            this.txtFiltter.TabIndex = 10;
+            // 
+            // comboBoxEdit1
+            // 
+            this.comboBoxEdit1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxEdit1.FormattingEnabled = true;
+            this.comboBoxEdit1.Items.AddRange(new object[] {
+            "全部",
+            "调试",
+            "信息",
+            "警告",
+            "错误"});
+            this.comboBoxEdit1.Location = new System.Drawing.Point(59, 14);
+            this.comboBoxEdit1.Name = "comboBoxEdit1";
+            this.comboBoxEdit1.Size = new System.Drawing.Size(111, 20);
+            this.comboBoxEdit1.TabIndex = 9;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(252, 18);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(53, 12);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "清空日志";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(5, 17);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(59, 14);
+            this.labelControl1.TabIndex = 7;
+            this.labelControl1.Text = "过滤级别";
+            // 
+            // checkEdit1
+            // 
+            this.checkEdit1.Location = new System.Drawing.Point(176, 15);
+            this.checkEdit1.Name = "checkEdit1";
+            this.checkEdit1.Size = new System.Drawing.Size(75, 19);
+            this.checkEdit1.TabIndex = 6;
+            this.checkEdit1.Text = "暂停显示";
+            // 
+            // UcLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupControl1);
+            this.Name = "UcLog";
+            this.Size = new System.Drawing.Size(593, 386);
+            this.Load += new System.EventHandler(this.UcLog_Load);
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private GroupBox groupControl1;
+        private CheckBox checkEdit1;
+        private Label labelControl1;
+        private LinkLabel linkLabel1;
+        private ComboBox comboBoxEdit1;
+        private TextBox txtFiltter;
+        private ToolTip toolTip1;
+        private Label label2;
+        private Label label1;
+        private TextBox txtFiltter2;
+    }
+}
---
+++ UcLog.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTUc
+{
+    /// <summary>
+    /// 日志控件
+    /// </summary>
+    public partial class UcLog : UserControl
+    {
+        private bool IsStop = false;
+        private UInt16 _logQueueSize;
+
+        private ConcurrentQueue<LogModel> _logQueue;
+
+        private Dictionary<string, int> _logIntDic = new Dictionary<string, int>();
+
+        public UcLog()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Init(UInt16 logQueueSize = 500, int index = 0)
+        {
+            if (logQueueSize == 0)
+            {
+                throw new Exception("日志队列不能设置为0");
+            }
+            IsStop = false;
+            _logQueueSize = logQueueSize;
+            _logQueue = new ConcurrentQueue<LogModel>();
+            _logIntDic.Add("全部", 0);
+            _logIntDic.Add("调试", 1);
+            _logIntDic.Add("信息", 2);
+            _logIntDic.Add("警告", 3);
+            _logIntDic.Add("错误", 4);
+            comboBoxEdit1.SelectedIndex = index;
+        }
+
+        public void UnInit()
+        {
+            IsStop = true;
+        }
+        private void UcLog_Load(object sender, EventArgs e)
+        {
+
+            Task.Factory.StartNew(GetMsg);
+        }
+
+        private void GetMsg()
+        {
+            while (!IsStop)
+            {
+                if (_logQueue != null && _logQueue.Count > 0)
+                {
+                    var item = new LogModel();
+                    if (_logQueue.TryDequeue(out item))
+                    {
+                        UpdateUi(item);
+                    }
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        public void AddMsg(ELogLevel logLevel, string msg)
+        {
+            var filterStr = txtFiltter.Text;
+            var filterStr2 = txtFiltter2.Text;
+            var filterLevel = comboBoxEdit1.Text;//默认全部
+            var levelStr = "全部";
+
+            var color = SystemColors.WindowText;
+            switch (logLevel)
+            {
+                case ELogLevel.DEBUG:
+                    levelStr = "调试";
+                    color = Color.Black;
+                    break;
+                case ELogLevel.INFO:
+                    levelStr = "信息";
+                    color = SystemColors.WindowText;
+                    break;
+                case ELogLevel.WARN:
+                    levelStr = "警告";
+                    color = Color.DarkGoldenrod;
+                    break;
+                case ELogLevel.ERROR:
+                    levelStr = "错误";
+                    color = Color.Red;
+                    break;
+                default:
+                    break;
+            }
+            var s = String.Format("[{0}][{1}]:{2}", levelStr, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), msg);
+            if (!checkEdit1.Checked)
+            {
+                if (_logIntDic[filterLevel] <= _logIntDic[levelStr])
+                {
+                    if (_logQueue.Count < _logQueueSize)
+                    {
+                        if (s.Contains(filterStr) && s.Contains(filterStr2))
+                        {
+                            _logQueue.Enqueue(new LogModel() { MColor = color, MMsg = s });
+                            Thread.Sleep(1);
+                        }
+
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+            }
+
+
+        }
+
+        private void UpdateUi(LogModel item)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new MethodInvoker(delegate
+                {
+                    if (richTextBox1.Lines.Length > _logQueueSize)
+                    {
+                        richTextBox1.ResetText();
+                    }
+                    AppendTextColorful(richTextBox1, item.MMsg, item.MColor);
+                }));
+            }
+            else
+            {
+                if (richTextBox1.Lines.Length > _logQueueSize)
+                {
+                    richTextBox1.ResetText();
+                }
+                AppendTextColorful(richTextBox1, item.MMsg, item.MColor);
+            }
+
+        }
+
+        private void AppendTextColorful(RichTextBox rtBox, string text, Color color, bool addNewLine = true)
+        {
+            if (addNewLine)
+            {
+                text += Environment.NewLine;
+            }
+            rtBox.SelectionStart = rtBox.TextLength;
+            rtBox.SelectionLength = 0;
+            rtBox.SelectionColor = color;
+            rtBox.AppendText(text);
+            rtBox.SelectionColor = rtBox.ForeColor;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.richTextBox1.ResetText();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length; //Set the current caret position at the end
+            richTextBox1.ScrollToCaret(); //Now scroll it automatically
+        }
+
+    }
+
+    public class LogModel
+    {
+        public Color MColor { get; set; }
+        public string MMsg { get; set; }
+    }
+
+    /// <summary>
+    /// 日志级别
+    /// </summary>
+    public enum ELogLevel
+    {
+        /// <summary>
+        /// 调试
+        /// </summary>
+        DEBUG,
+        /// <summary>
+        /// 信息
+        /// </summary>
+        INFO,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        WARN,
+        /// <summary>
+        /// 错误
+        /// </summary>
+        ERROR
+    }
+}
